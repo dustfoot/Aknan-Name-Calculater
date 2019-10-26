b7ff308fa9d806150119b54b231273d95b118115
dustfoot <dustfoot3668@gmail.com>
2019-10-26 15:28:31 +0300

    creating the function of the variables

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
 // alert("hi, from js"); testing js
 var century, year, month, dayOfMonth, dayOfWeek, day;
 
+//Get input
+function getInput(){
+    century=parseInt(document.getElementById("century").value);
+    year=parseInt(document.getElementById("year").value);
+    month=parseInt(document.getElementById("month").value);
+    dayOfMonth=parseInt(document.getElementById("monthday").value);
+}
+
