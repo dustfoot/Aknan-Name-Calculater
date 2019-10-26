8b8bc572ea5959a602f6c55b8d4ff4c110090c98
dustfoot <dustfoot3668@gmail.com>
2019-10-26 15:39:00 +0300

    adding the calculate-day function

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ function getInput(){
     year=parseInt(document.getElementById("year").value);
     month=parseInt(document.getElementById("month").value);
     dayOfMonth=parseInt(document.getElementById("monthday").value);
-}
 
 if(century == ""){
     alert("Input the correct gender");
@@ -23,4 +22,20 @@ if(century == ""){
     return false;
   }
 }
+
+
+function calculateDay(){
+    getInput();
+    dayOfWeek = ((((century/4) -2*century-1) + ((5*year/4) ) + ((26*(month+1)/10)) + dayOfMonth) % 7) -1;
+    console.log(dayOfWeek);
+    return (Math.floor(dayOfWeek));
+    if (dayOfWeek < 0) {
+      dayOfWeek = dayOfWeek * -1;
+    }
+    else if (dayOfWeek > 0) {
+      return dayOfWeek;
+    }
+}
+
+
 
